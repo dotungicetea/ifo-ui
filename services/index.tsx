6d4aa8f0090eea338d2a45c2b2b89eb355a36ba4
domanhtung <tung.do@icetea.io>
2022-10-17 09:54:22 +0700

    update api

--- services/index.tsx
+++ services/index.tsx
@@ -3,7 +3,7 @@ import { jsonParse } from '../utils'
 
 export const connectWalletApi = (data: any) => {
     const dataJson = jsonParse(data)
-    axios.post('http://54.227.118.34:8080/api/v1/user/register', dataJson).then(res => {
+    axios.post('https://ifo.icetea.io/api/v1/user/register', dataJson).then(res => {
         console.log(res)
     }).catch(e => {
         console.log(e)
